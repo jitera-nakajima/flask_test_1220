44c06b954a66e4fdd06e4dad40c1af286962213e
jitera-nakajima <yusuke.nakajima@jitera.com>
2024-12-20 14:36:16 +0900

    debug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
-from controllers.subscription_controller import SubscriptionController
+from src.controllers.subscription_controller import SubscriptionController
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,7 @@ subscription_controller = SubscriptionController()
 # POSTリクエストで購読を作成するエンドポイントを設定します
 @app.route('/api/subscription', methods=['POST'])
 def create_subscription():
+    print("Received request for subscription creation")  # デバッグ用のログ
     return subscription_controller.create()
 
 if __name__ == '__main__':
